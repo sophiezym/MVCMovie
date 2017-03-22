3ce9ceb397898c74100627d531437a77037d67f2
sophiezym <zhouy315@myumanitoba.ca>
2017-03-21 21:32:28 -0500

    Adding search function
    Create searchIndex function in Movies controller and create view for
    searchIndex

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -119,5 +119,18 @@ namespace MvcMovie.Controllers
             db.Dispose();
             base.Dispose(disposing);
         }
+
+        public ActionResult SearchIndex(string searchString)
+        {
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(movies);
+        }
     }
 }
